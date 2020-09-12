3594c99ce230e82bc003b02a62e0d35d5a279d48
Swapnil Gusani <swapniel99@gmail.com>
2020-09-12 20:06:28 +0530

    new network model7

--- ezio/model/session_7/model.py
+++ ezio/model/session_7/model.py
@@ -5,13 +5,11 @@ class Net_7(nn.Module):
     def __init__(self):
         super(Net_7, self).__init__()
         self.cblock1 = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=0, bias=False),
-            nn.BatchNorm2d(64),
-            #nn.Dropout(DROP),
+            nn.Conv2d(3, 128, 3, padding=0, bias=False),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(64, 64, 3, padding=0, bias=False),
-            nn.BatchNorm2d(64),
-            #nn.Dropout(DROP),
+            nn.Conv2d(128, 128, 3, padding=0, bias=False),
+            nn.BatchNorm2d(128),
             nn.ReLU()
         )
 
@@ -20,15 +18,13 @@ class Net_7(nn.Module):
         )
 
         self.cblock2 = nn.Sequential(
-            nn.Conv2d(64, 64, 3, padding=1, bias=False, groups=64),
-            nn.Conv2d(64, 128, 1, padding=0, bias=False),
-            nn.BatchNorm2d(128),
-            #nn.Dropout(DROP),
+            nn.Conv2d(128, 128, 3, dilation=2, padding=2, bias=False, groups=128),
+            nn.Conv2d(128, 256, 1, padding=0, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.Conv2d(128, 128, 3, padding=1, bias=False, groups=128),
-            nn.Conv2d(128, 128, 1, padding=0, bias=False),
-            nn.BatchNorm2d(128),
-            #nn.Dropout(DROP),
+            nn.Conv2d(256, 256, 3, dilation=2, padding=2, bias=False, groups=256),
+            nn.Conv2d(256, 256, 1, padding=0, bias=False),
+            nn.BatchNorm2d(256),
             nn.ReLU()
         )
 
@@ -37,29 +33,26 @@ class Net_7(nn.Module):
         )
 
         self.cblock3 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, dilation=2, padding=2, bias=False),
-            nn.BatchNorm2d(256),
-            #nn.Dropout(DROP),
+            nn.Conv2d(256, 256, 3, padding=1, bias=False, groups=256),
+            nn.Conv2d(256, 512, 1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.Conv2d(256, 256, 3, dilation=2, padding=2, bias=False),
-            nn.BatchNorm2d(256),
-            #nn.Dropout(DROP),
+            nn.Conv2d(512, 512, 3, padding=1, bias=False, groups=512),
+            nn.Conv2d(512, 512, 1, padding=0, bias=False),
+            nn.BatchNorm2d(512),
+
             nn.ReLU()
         )
 
         self.outblock = nn.Sequential(
             nn.AvgPool2d(7, 7),
-            nn.BatchNorm2d(256),
-            nn.Conv2d(256, 128, 1, bias=False),
-            #nn.Dropout(0.0),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 512, 1, bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.Conv2d(128, 10, 1),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 10, 1),
             nn.Flatten()
-            #nn.LogSoftmax()
         )
-
-
 
     def forward(self, x):
         x = self.cblock1(x)
