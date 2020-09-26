8e3859659259efe60922e53133fa059fdc64945d
karanpatel1993 <karan.patel@pharmeasy.in>
2020-09-26 18:46:12 +0530

    plot gradcam images

--- ezio/data/data_transforms/session_9/transformations.py
+++ ezio/data/data_transforms/session_9/transformations.py
@@ -2,21 +2,29 @@ import albumentations as A
 from albumentations.pytorch import ToTensor
 import numpy as np
 
+# Calculate mean and std of cifar-10 dataset
+mean = [0.49139968, 0.48215841, 0.44653091]
+std = [0.24703223, 0.24348513, 0.26158784]
+
+
+def denormalize_image(img):
+    img = img.numpy().astype(dtype=np.float32)
+    for i in range(img.shape[0]):
+        img[i] = (img[i] * std[i]) + mean[i]
+
+    return np.transpose(img, (1, 2, 0))
+
 
 def perform_transformations(train=False, is_numpy=False):
     # Initialize transforms list
     final_transforms = list()
-
-    # Calculate mean and std of cifar-10 dataset
-    mean = [0.49139968, 0.48215841, 0.44653091]
-    std = [0.24703223, 0.24348513, 0.26158784]
 
     # Apply transformations only for training dataset
     if train:
         albumentation_transforms = [
             A.ShiftScaleRotate(rotate_limit=7, shift_limit = (0.1, 0.1), scale_limit=(0.9, 1.1)),
             A.HorizontalFlip(p=0.5),
-            A.Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=mean*255)
+            A.Cutout(num_holes=1, max_h_size=16, max_w_size=16)
             ]
         final_transforms += albumentation_transforms
 
