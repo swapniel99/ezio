50c47ae6fc907be473c44df15fd28cf11a9da152
karanpatel1993 <karan.patel@pharmeasy.in>
2020-10-10 19:17:06 +0530

    trainer and engine bug fix

--- ezio/train/trainer.py
+++ ezio/train/trainer.py
@@ -1,28 +1,23 @@
 import torch
 from tqdm import tqdm
 
-
-def train(model, device, train_loader, optimizer, criterion, l1_loss=False, lambda_l1=None):
+def train(model, device, train_loader, optimizer, criterion, l1_loss=False, lambda_l1=None, scheduler=None):
     train_losses = []
     train_acc = []
+    learning_rate = []
+
     model.train()
     pbar = tqdm(train_loader)
+
     correct = 0
     processed = 0
     for batch_idx, (data, target) in enumerate(pbar):
-        # get samples
         data, target = data.to(device), target.to(device)
 
         # Init
         optimizer.zero_grad()
-        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes.
-        # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
 
-        # Predict
         y_pred = model(data)
-
-        # Calculate loss
-        # criterion = nn.CrossEntropyLoss()
         loss = criterion(y_pred, target)
         if l1_loss:
             l1 = 0
@@ -31,18 +26,17 @@ def train(model, device, train_loader, optimizer, criterion, l1_loss=False, lamb
             loss = loss + lambda_l1 * l1
         train_losses.append(loss)
 
-        # Backpropagation
         loss.backward()
         optimizer.step()
+        if scheduler:
+            scheduler.step()
 
-        # Update pbar-tqdm
-
-        # pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-        # correct += pred.eq(target.view_as(pred)).sum().item()
-        _, preds = torch.max(y_pred, 1)  # taking the highest value of prediction.
-        correct += torch.sum(preds == target.data)  # calculating te accuracy by taking the sum of all the correct predictions in a batch.
+        _, preds = torch.max(y_pred, 1)
+        correct += torch.sum(preds == target.data)
         processed += len(data)
 
-        pbar.set_description(desc=f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={(100 * correct // processed):0.2f}')
+        pbar.set_description(
+            desc=f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={(100 * correct // processed):0.2f} Learning Rate={optimizer.state_dict()["param_groups"][0]["lr"]}')
         train_acc.append(100 * correct // processed)
-    return train_acc, train_losses
+        learning_rate.append(optimizer.state_dict()["param_groups"][0]["lr"])
+    return train_acc, train_losses, learning_rate
